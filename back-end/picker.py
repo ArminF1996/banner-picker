b9841939eed42796a7170d2012327398f52fa7c7
Armin Fallah <armin.f1996@gmail.com>
2023-03-22 11:43:21 +0330

    Get username from client to show banner based on their history

--- back-end/picker.py
+++ back-end/picker.py
@@ -1,8 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template , request, redirect, url_for
 
 bp = Blueprint("picker", __name__)
 
 
-@bp.route("/pick", methods=['GET'])
+@bp.route("/pick", methods=['GET', 'POST'])
 def pick_banner():
-    return render_template('show_banner.html')
+    if request.method == 'GET':
+        return render_template('show_banner.html')
+    else:
+        username = request.values['username']
+        return redirect(url_for('picker.show_banner', username=username))
+
+
+@bp.route("/pick/<string:username>", methods=['GET'])
+def show_banner(username):
+    print(username)
+    image_path = 'images/image_100.jpeg'
+    return render_template('show_banner.html', image_path=image_path)
