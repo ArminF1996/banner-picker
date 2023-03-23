c3329d6019377904f1f85e9f698091ca11acd20b
Armin Fallah <armin.f1996@gmail.com>
2023-03-23 15:38:50 +0330

    Add revision for database data  + add Dockerfile for etl module

--- etl/etl/models.py
+++ etl/etl/models.py
@@ -41,7 +41,7 @@ class Impression(Base):
 
 
 def import_impressions(logfile, data, quarter):
-    with open(logfile, 'r+') as f:
+    with open(logfile, 'w+') as f:
         f.truncate(0)
         total_added = 0
         cur_line = 0
@@ -51,6 +51,8 @@ def import_impressions(logfile, data, quarter):
             next(csvreader)
             for row in csvreader:
                 cur_line += 1
+                if cur_line % 10000 == 0:
+                    logger.info("{} impression records processed by now.".format(cur_line))
                 banner_id = int(row[0])
                 campaign_id = int(row[1])
 
@@ -61,6 +63,12 @@ def import_impressions(logfile, data, quarter):
                 else:
                     total_added += 1
                     entities.append(Impression(quarter=quarter, campaign_id=campaign_id, banner_id=banner_id))
+
+            if total_added > 0:
+                revision = session.query(Revision).first()
+                revision.revision += 1
+                entities.append(revision)
+
             session.bulk_save_objects(entities)
             session.commit()
         result = "{0} from {1} rows successfully added to database.".format(total_added, cur_line)
@@ -86,14 +94,14 @@ class Click(Base):
 
     def __repr__(self) -> str:
         return f"Click(click_id={self.click_id!r}, campaign_id={self.campaign_id!r}, banner_id={self.banner_id!r}, " \
-               f"quarter={self.quarter!r}"
+               f"quarter={self.quarter!r})"
 
 
 def import_clicks(logfile, data, quarter):
     new_clicks = []
-    with open(logfile, 'r+') as f:
+    total_added = 0
+    with open(logfile, 'w+') as f:
         f.truncate(0)
-        total_added = 0
         cur_line = 0
         with Session() as session:
             entities = []
@@ -101,6 +109,8 @@ def import_clicks(logfile, data, quarter):
             next(csvreader)
             for row in csvreader:
                 cur_line += 1
+                if cur_line % 10000 == 0:
+                    logger.info("{} click records processed by now.".format(cur_line))
                 click_id = int(row[0])
                 banner_id = int(row[1])
                 campaign_id = int(row[2])
@@ -122,7 +132,8 @@ def import_clicks(logfile, data, quarter):
         result = "{0} from {1} rows successfully added to database.".format(total_added, cur_line)
         f.write(result + "\nThe import process has finished.\n")
         logger.info(result)
-    update_with_clicks(new_clicks)
+    if total_added > 0:
+        update_with_clicks(new_clicks)
 
 
 def update_with_clicks(clicks):
@@ -142,7 +153,12 @@ def update_with_clicks(clicks):
             impression = session.query(Impression).get(k)
             impression.total_clicks += v
             entities.append(impression)
-        session.add_all(entities)
+
+        revision = session.query(Revision).first()
+        revision.revision += 1
+        entities.append(revision)
+
+        session.bulk_save_objects(entities)
         session.commit()
 
 
@@ -164,14 +180,14 @@ class Conversion(Base):
 
     def __repr__(self) -> str:
         return f"Conversion(conversion_id={self.conversion_id!r}, click_id={self.click_id!r}, " \
-               f"revenue={self.revenue!r}), quarter={self.quarter!r}"
+               f"revenue={self.revenue!r}, quarter={self.quarter!r})"
 
 
 def import_conversions(logfile, data, quarter):
     new_conversions = {}
-    with open(logfile, 'r+') as f:
+    total_added = 0
+    with open(logfile, 'w+') as f:
         f.truncate(0)
-        total_added = 0
         cur_line = 0
         with Session() as session:
             entities = []
@@ -179,6 +195,8 @@ def import_conversions(logfile, data, quarter):
             next(csvreader)
             for row in csvreader:
                 cur_line += 1
+                if cur_line % 10000 == 0:
+                    logger.info("{} conversion records processed by now.".format(cur_line))
                 conversion_id = int(row[0])
                 click_id = int(row[1])
                 revenue = float(row[2])
@@ -199,7 +217,8 @@ def import_conversions(logfile, data, quarter):
         result = "{0} from {1} rows successfully added to database.".format(total_added, cur_line)
         f.write(result + "\nThe import process has finished.\n")
         logger.info(result)
-    update_with_conversions(new_conversions)
+    if total_added > 0:
+        update_with_conversions(new_conversions)
 
 
 def update_with_conversions(conversions):
@@ -207,6 +226,7 @@ def update_with_conversions(conversions):
         data = {}
         for k, v in conversions.items():
             click = session.query(Click).get(k)
+            # TODO: handling conversions that their click doesn't exist
             if click is None:
                 continue
             impression = session.query(Impression).filter_by(quarter=click.quarter, campaign_id=click.campaign_id,
@@ -221,8 +241,29 @@ def update_with_conversions(conversions):
             impression = session.query(Impression).get(k)
             impression.total_revenue += v
             entities.append(impression)
-        session.add_all(entities)
+
+        revision = session.query(Revision).first()
+        revision.revision += 1
+        entities.append(revision)
+
+        session.bulk_save_objects(entities)
         session.commit()
+
+
+class Revision(Base):
+    __tablename__ = "revision"
+
+    id = Column(Integer, nullable=False, primary_key=True)
+    revision = Column(Integer, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'revision': self.revision
+        }
+
+    def __repr__(self) -> str:
+        return f"Revision(id={self.id!r}, revision={self.revision!r})"
 
 
 def create_db_connection(configs):
@@ -236,3 +277,10 @@ def create_db_connection(configs):
     global Session
     Session = sessionmaker(bind=engine)
     Base.metadata.create_all(engine, checkfirst=True)
+
+    with Session() as session:
+        revision = session.query(Revision).first()
+        if revision is None:
+            revision = Revision(revision=0)
+            session.add(revision)
+            session.commit()
