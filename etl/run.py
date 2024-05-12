a1e8d31def159ed211c8e729b2a74271b25109a5
Armin <armin.f1996@gmail.com>
2024-05-12 12:10:24 +0330

    test sonar

--- etl/run.py
+++ etl/run.py
@@ -3,7 +3,7 @@ from etl import run
 import sys
 
 if len(sys.argv) < 2:
-    raise ValueError("Please enter config.ini path as an argument.")
+    raise ValueError("Enter config.ini path as an argument.")
 config = get_config(sys.argv[1])
 
 run(config)
