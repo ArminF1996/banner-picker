8e9e1bc55f9b1901e40f3c6a3ac1749951a760a9
Armin <armin.f1996@gmail.com>
2024-05-13 15:36:17 +0330

    test sonar

--- back-end/run.py
+++ back-end/run.py
@@ -5,7 +5,7 @@ from config import get_config
 import sys
 
 if len(sys.argv) < 2:
-    raise ValueError("Please enter config.ini path as an argument.")
+    raise ValueError("Please Enter config.ini path as an argument.")
 config = get_config(sys.argv[1])
 
 serve(TransLogger(create_app(config)), host=config.HOST, port=config.PORT, threads=config.APP_THREADS)
